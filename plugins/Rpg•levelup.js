a54c35bd3d78587f4b31bdfc520492754fda354d
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-27 08:21:52 -0400

    Actualizar Rpg•levelup.js

--- plugins/Rpg•levelup.js
+++ plugins/Rpg•levelup.js
@@ -7,18 +7,18 @@ let name = conn.getName(m.sender)
 let user = global.db.data.users[m.sender]
 if (!canLevelUp(user.level, user.exp, global.multiplier)) {
 let { min, xp, max } = xpRange(user.level, global.multiplier)
-let txt = `🩵 *Nombre* ${name}\n\n`
-txt += `🩵 *Nivel* ${user.level}\n`
-txt += `🩵 *XP* ${user.exp - min} / ${xp}\n\n`
-txt += `🩵 No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
+let txt = `⚡ *Nombre* ${name}\n\n`
+txt += `⚡*Nivel* ${user.level}\n`
+txt += `⚡*XP* ${user.exp - min} / ${xp}\n\n`
+txt += `⚡ No es suficiente XP *${max - user.exp}* ¡De nuevo! ✨`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, fake)}
 let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 if (before !== user.level) {
 let txt = `🎊 F E L I C I T A C I O N E S 🎊\n\n` 
 txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
-txt += `• 🩵 Nivel anterior : ${before}\n`
-txt += `• 🩵 Nuevos niveles : ${user.level}\n`
+txt += `• ⚡ Nivel anterior : ${before}\n`
+txt += `• ⚡ Nuevos niveles : ${user.level}\n`
 txt += `• 📅 Fecha : ${new Date().toLocaleString('id-ID')}\n\n`
 txt += `🚩 *Nota:* _Mientras más uses a  *Makima*, podrás ganar más nivel._`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, fake)}}

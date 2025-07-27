26d1dfc15ee703558491a11177efbfef67112ff5
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-07-27 15:58:05 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -16,7 +16,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
   const jid = m.messageStubParameters[0]
   const user = `@${jid.split('@')[0]}`
-  const thumbnailUrl = 'https://files.catbox.moe/y3gmmd.png'
+  const thumbnailUrl = 'https://qu.ax/GszaU.png'
   const pp = await conn.profilePictureUrl(jid, 'image').catch(() => thumbnailUrl)
   const img = await fetch(pp).then(r => r.buffer())
   const total = [28, 32].includes(m.messageStubType)

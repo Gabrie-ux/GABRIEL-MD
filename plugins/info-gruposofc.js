96f6a195fa78684338d4b58ccf59e4a357b5a020
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-07-27 19:02:33 -0400

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
 
   const namegrupo = 'Grupo Oficial'
-  const gp1 = 'https://chat.whatsapp.com/KoJjHo6o3Ew7P5qkjaIh0r' // ← tu link real
+  const gp1 = 'https://chat.whatsapp.com/Fl22Fx2sMatAe1jDN8lk9y?mode=ac_t' // ← tu link real
 
   const namechannel = 'Canal de la Bot'
   const channel = 'https://whatsapp.com/channel/0029VbAZcyIIXnlwp79iwu2l' // ← tu canal real

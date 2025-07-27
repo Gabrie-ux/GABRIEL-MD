8aedf8118bbf385d9ad5d91b4f49f8edc8523824
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-26 23:29:32 -0400

    Actualizar jadibot-primary.js

--- plugins/jadibot-primary.js
+++ plugins/jadibot-primary.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, text }) => {
 
   if (!text) throw 'Etiqueta al bot que será el principal o pasa su número'
 
-  
+
   let botJid = text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
 
   if (!global.db.data.chats[m.chat]) global.db.data.chats[m.chat] = {}
 
   global.db.data.chats[m.chat].primaryBot = botJid
 
-  m.reply(`✅ El bot principal para este grupo ahora es:\n*${botJid}*`)
+  m.reply(`El bot principal para este grupo ahora es:\n*${botJid}*`)
 }
 
 handler.help = ['setprimary @bot']

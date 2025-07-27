c64d45210ec3230654cc761a4c84ed5c593099c6
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2025-07-27 08:27:52 -0400

    Actualizar Owner•leavegc.js

--- plugins/Owner•leavegc.js
+++ plugins/Owner•leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `「🩵」MAKIMA SE DESPIDE, FUE INCREÍBLE CONOCERLOS.`) 
+await conn.reply(id, `「⚡」ELISABET SE DESPIDE, FUE INCREÍBLE CONOCERLOS🥲.`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true

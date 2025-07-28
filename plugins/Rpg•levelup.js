20bd0975427f2c07ce1d74d0db6c6815e7c5be1b
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-07-27 20:07:30 -0400

    Actualizar Rpg•levelup.js

--- plugins/Rpg•levelup.js
+++ plugins/Rpg•levelup.js
@@ -20,7 +20,7 @@ txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
 txt += `• ⚡ Nivel anterior : ${before}\n`
 txt += `• ⚡ Nuevos niveles : ${user.level}\n`
 txt += `• 📅 Fecha : ${new Date().toLocaleString('id-ID')}\n\n`
-txt += `🚩 *Nota:* _Mientras más uses a  *Makima*, podrás ganar más nivel._`
+txt += `🚩 *Nota:* _Mientras más uses a  *Elisabet*, podrás ganar más nivel._`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, fake)}}
 
 handler.help = ['levelup']

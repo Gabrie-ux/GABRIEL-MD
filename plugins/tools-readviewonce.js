fbd0fcbf8570759433b75ef7949e38cc4a584987
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-08-24 16:36:49 -0400

    Crear tools-readviewonce.js

---
+++ plugins/tools-readviewonce.js
@@ -0,0 +1,20 @@
+// Créditos A Destroy (https://github.com/The-king-Destroy)
+
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
+
+let handler = async (m, { conn }) => {
+if (!m.quoted) return conn.reply(m.chat, `《★》Responde a una imagen ViewOnce.`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `《★》Responde a una imagen ViewOnce.`, m)
+let buffer = await m.quoted.download(false);
+if (/videoMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
+} else if (/imageMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
+}}
+
+handler.help = ['ver']
+handler.tags = ['tools']
+handler.command = ['readviewonce', 'read', 'readvo', 'ver'] 
+// handler.register = false 
+
+export default handler
